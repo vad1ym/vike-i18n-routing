cab4317b848b31c7c9f08478fe6d67d86457ffa8
Vadym Bulakh <bulakhvad@gmail.com>
2026-05-21 10:29:56 +0300

    test: add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,19 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['lib/**/*.ts'],
+      exclude: [
+        'lib/**/*.d.ts',
+        'lib/**/tsconfig.json',
+        'lib/react/**',
+        'lib/solid/**',
+        'lib/vue/**',
+        'lib/vike/global.d.ts',
+      ],
+    },
   },
 })
